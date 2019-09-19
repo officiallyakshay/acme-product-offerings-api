8176489cfeeaf69408e875423192724ba732f905
Alexandra Pini <alexandrampini@gmail.com>
2019-09-18 22:48:28 -0400

    second commit

--- db.js
+++ db.js
@@ -4,5 +4,5 @@ const { STRING, UUID, UUIDV4 } = Sequelize;
 const conn = new Sequelize( process.env.DATABASE_URL || 'postgres://localhost/acme_product_offerings_api');
 
 const sync = async () => {
-  
+  await conn.sync({force: true});
 }
