a02d5378ba4cd1a65f61d2d156257090eeb05d30
Alexandra Pini <alexandrampini@gmail.com>
2019-09-18 23:46:25 -0400

    res.send

--- db.js
+++ db.js
@@ -1,8 +1,84 @@
 const Sequelize = require('sequelize');
 const { STRING, UUID, UUIDV4 } = Sequelize;
-
 const conn = new Sequelize( process.env.DATABASE_URL || 'postgres://localhost/acme_product_offerings_api');
+
+
+const Product = conn.define('product', {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+    allowNull: false
+  },
+  name: STRING
+});
+
+const Company = conn.define('company', {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+    allowNull: false
+  },
+  name: STRING
+});
+
+const Offering = conn.define('offering', {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+    allowNull: false
+  },
+  name: STRING
+});
+
+Product.belongsTo(Company, {as: 'company'});
+
+Company.hasMany(Product, {foreignKey: 'productId'});
+
+Offering.belongsTo(Product, {as: 'product'});
+Offering.belongsTo(Company, {as: 'company'});
+
+Product.hasMany(Offering, {foreignKey: 'offeringId'});
+Company.hasMany(Offering, {foreignKey: 'offeringId'});
+
+const mapPromise = (items, model) => {
+  return Promise.all(items.map ( item => model.create(item)));
+}
+
 
 const sync = async () => {
   await conn.sync({force: true});
-}
+
+
+    const products = [
+      {name: 'foo'},
+      {name: 'bar'},
+      {name: 'bazz'},
+      {name: 'quq'}
+    ]
+
+    const companies = [
+      {name: 'The Foo Company'},
+      {name: 'The Bar Company'},
+      {name: 'The Bazz Company'},
+      {name: 'The Quq Company'}
+    ]
+
+    const offerings = [
+      {name: 'foo'},
+      {name: 'bar'},
+      {name: 'bazz'},
+      {name: 'quq'}
+    ]
+  };
+
+  module.exports = {
+    sync,
+    models: {
+      Product,
+      Company,
+      Offering
+    }
+};
--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 const express = require('express');
 const path = require('path');
-const db = require('./db');
 const port = process.env.PORT || 3000;
 const app = express();
+const db = require('./db');
+const { models: { Product, Company, Offering }, sync } = require('./db');
 
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.get('/api/products', (req, res, next) => {
-
+app.get('/api/products', async (req, res, next) => {
+  try {
+    res.send(await Product.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
 });
 
-app.get('/api/companies', (req, res, next) => {
-
+app.get('/api/companies', async (req, res, next) => {
+  try {
+    res.send(await Company.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
 });
 
-app.get('/api/offerings', (req, res, next) => {
-
+app.get('/api/offerings', async (req, res, next) => {
+  try {
+    res.send(await Offering.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
 });
 
-db.sync()
+sync()
   .then(() => {
       app.listen(port, () => console.log(`listening on port ${port}`));
   })
